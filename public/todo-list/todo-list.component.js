f785b2f67891ed69a5025f657f936133270ac41c
Dmitri Tcherevik <cherevik@yahoo.com>
2016-08-04 15:26:16 -0400

    added click handler

--- public/todo-list/todo-list.component.js
+++ public/todo-list/todo-list.component.js
@@ -4,5 +4,9 @@ component('todoList', {
     templateUrl: 'todo-list/todo-list.template.html',
     controller: function TodoListController($scope, Item) {
         $scope.items = Item.query();
+
+        $scope.itemClicked = function(item) {
+            console.log(item.id);
+        }
     }
 });
